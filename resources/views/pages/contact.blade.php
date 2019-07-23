40546b5e25603640aa19334bc6a0b1100de3d89d
Abdul-fatai <staticdev20046@gmail.com>
2019-07-23 19:49:14 +0100

    new update

--- resources/views/pages/contact.blade.php
+++ resources/views/pages/contact.blade.php
@@ -2,8 +2,10 @@
 
 @section('content')
     <div class="jumbotron">
-        <h1>Contact Us</h1>
-        <h3>Phone: +2348161846658</h3>
-        <h3>Email: bloggerbase@gmail.com</h3>
+        <h1 style="font-size: 50px">Contact</h1>
+        <h3>The Practical Nerdsweb would love to hear from you!</h3><br>
+        <h5>Email: Nerds@Nerdsweb.co</h5>
+        <h5>Twitter: bloggerbase@gmail.com</h5>
+        <h5>Please make a GitHub issue for feature requests: <a href="https://github.com/Abdul-fatai/Nerdsweb">Nerdsweb</a></h5>
     </div>
 @endsection
