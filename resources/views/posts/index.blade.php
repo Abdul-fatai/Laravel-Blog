fe13b2a157a70bb66586a2df578b46299d5eef2d
Abdul-fatai <staticdev20046@gmail.com>
2019-07-23 19:39:47 +0100

    new update

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -2,7 +2,9 @@
 
 @section('content')
 @include('inc.message')
-    <h1>POSTS</h1>
+    <div class="row">
+    <div class="col-md-8">
+        <h2>POSTS</h2>
     @if(count($posts) > 0)
         @foreach ($posts as $post)
         <div class="mb-2 card bg-white">
@@ -25,11 +27,34 @@
             </div>
         </div>
         </div>
+  
         @endforeach
         {{$posts->links()}}
     @else
         <p>No Post Found</p>
     @endif
+    </div>
+    <div class="col-md-4">
+            <h2>Popular Articles</h2> 
+            <div class="mb-2 card bg-white">
+                    <div class="card-body">
+                        <div class="row">
+                        <div class="col-md-12 col-sm-12">
+                            <a href="/posts/{{$post->id}}"><h3>{{$post->title}}</h3></a>
+                            <div class="badge bg-dark">
+                                {{$post->tag_name}}
+                            </div>
+                            <div style="height:50px; overflow: hidden">
+                                {!!$post->body!!}
+                            </div>
+                            <small>Written at {{$post->created_at}} by {{$post->user->name}}</small><br>
+                            <a class="btn btn-primary" href="/posts/{{$post->id}}">Read More</a>
+                        </div>
+                    </div>
+                </div>
+                </div>
+        </div>
+    </div>
 @endsection
 
 
