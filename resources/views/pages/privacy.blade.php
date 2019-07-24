f3397f2fc52dea393aeffe79886c32812d8b39b3
Abdul-fatai <staticdev20046@gmail.com>
2019-07-24 09:57:10 +0100

    new update

--- resources/views/pages/privacy.blade.php
+++ resources/views/pages/privacy.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
 
-    <div class="jumbotron">
+    <div class="jumbotron mt-4">
         <h1>Privacy</h1>
         <p>Loremp impsus is a generated random text. Loremp impsus is a generated random text.
              Loremp impsus is a generated random text. Loremp impsus is a generated random text. 
